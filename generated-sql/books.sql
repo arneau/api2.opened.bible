c563183f0666277dcc420e1f9e62dcdae00fc59d
Arneau van der Horst <github@arneau.me>
2017-04-17 20:34:07 +0200

    Commit

---
+++ generated-sql/books.sql
@@ -0,0 +1,37 @@
+-- MySQL dump 10.13  Distrib 5.6.27, for osx10.10 (x86_64)
+--
+-- Host: localhost    Database: api.opened.bible
+-- ------------------------------------------------------
+-- Server version	5.6.27
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Dumping data for table `book`
+--
+
+LOCK TABLES `book` WRITE;
+/*!40000 ALTER TABLE `book` DISABLE KEYS */;
+INSERT INTO `book` VALUES ('Genesis',1),('Exodus',2),('Leviticus',3),('Numbers',4),('Deuteronomy',5),('Joshua',6),('Judges',7),('Ruth',8),('1 Samuel',9),('2 Samuel',10),('1 Kings',11),('2 Kings',12),('1 Chronicles',13),('2 Chronicles',14),('Ezra',15),('Nehemiah',16),('Esther',17),('Job',18),('Psalms',19),('Proverbs',20),('Ecclesiastes',21),('Song of Songs',22),('Isaiah',23),('Jeremiah',24),('Lamentations',25),('Ezekiel',26),('Daniel',27),('Hosea',28),('Joel',29),('Amos',30),('Obadiah',31),('Jonah',32),('Micah',33),('Nahum',34),('Habakkuk',35),('Zephaniah',36),('Haggai',37),('Zechariah',38),('Malachi',39),('Matthew',40),('Mark',41),('Luke',42),('John',43),('Acts',44),('Romans',45),('1 Corinthians',46),('2 Corinthians',47),('Galatians',48),('Ephesians',49),('Philippians',50),('Colossians',51),('1 Thessalonians',52),('2 Thessalonians',53),('1 Timothy',54),('2 Timothy',55),('Titus',56),('Philemon',57),('Hebrews',58),('James',59),('1 Peter',60),('2 Peter',61),('1 John',62),('2 John',63),('3 John',64),('Jude',65),('Revelation',66);
+/*!40000 ALTER TABLE `book` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-04-17 20:31:21
